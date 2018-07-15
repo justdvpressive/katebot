d9e16c6c366b0486939c2a0565bb03a99376ec03
Hayden Young <hayden@haydenbjyoung.me>
2018-07-15 21:59:56 +0100

    Created basic snippets command

---
+++ commands/dev/snippets.js
@@ -0,0 +1,62 @@
+const {  Command  } = require('discord.js-commando');
+const  EmbedFooter  = require('../../util/embedFooter');
+
+module.exports = class Command extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'snippets',
+      group: 'dev',
+      memberName: 'snippets',
+      description: 'Snippets command!',
+      args: [
+        {
+          key: 'method',
+          prompt: '<get|remove|new|edit>',
+          type: 'string'
+        },
+        {
+          key: 'title',
+          prompt: 'Title of the snippet',
+          type: 'string'
+        },
+        {
+          key: 'content',
+          prompt: 'Content of the snippet',
+          type: 'string',
+          default: ''
+        },
+      ]
+    });
+  }
+
+  run(msg, { method, title, content }) {
+    switch(method) {
+      case 'get':
+        const snip = this.client.settings.get(msg.guild, title);
+        msg.say({ embed: {
+          title: `\`${title}\``,
+          description: (!snip.beginsWith('```')) ? '```' + snip + '```' : snip,
+          footer: {
+            icon: this.client.user.avatarURL,
+            text: EmbedFooter()
+          },
+          color: 0xFE5B35
+        } })
+        break;
+      case 'new':
+        /* **Example of a snippet key**
+          'snippet-example': '```\nMeep!\n```'
+        */
+        this.client.settings.set(msg.guild, 'snippet-' + title, content);
+        msg.say({ embed: {
+          title: 'Snippet created!',
+          description: 'Snippet made with the title of `' + title + '`.',
+          footer: {
+            icon: this.client.user.avatarURL,
+            text: EmbedFooter()
+          },
+          color: 0xFE5B35
+        }})
+    }
+  }
+};
