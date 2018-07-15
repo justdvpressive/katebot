125ecda640ac7339e20ae9116f1f6c6e550c2401
Hayden Young <hayden@haydenbjyoung.me>
2018-07-16 00:21:24 +0100

    Also totally did *not* forget about how find() works. Nooooope.

--- commands/util/suggest.js
+++ commands/util/suggest.js
@@ -28,7 +28,7 @@ module.exports = class SuggestCommand extends Command {
       .setFooter(EmbedFooter(), this.client.user.avatarURL)
       .setTimestamp(new Date().now);
     
-    const channel = await msg.guild.channels.find('suggestions');
+    const channel = await msg.guild.channels.find('name', 'suggestions');
     channel.send({ embed }).then(message => {
       message.react('👍');
       message.react('👎');
