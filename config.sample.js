8529910703183b782eda0496b2964b0accf72c2d
Hayden Young <hayden@haydenbjyoung.me>
2018-07-15 18:11:43 +0100

    Updated file format of config.js

--- config.sample.js
+++ config.sample.js
@@ -1,16 +1,14 @@
-var { token, prefix, owner, name, firebaseApiKey } = process.env;
-
 // This config.js file is used for Heroku.
 // Learn more about this at https://devcenter.heroku.com/articles/config-vars,
 // or you can set the values as strings.
 module.exports = {
   discord: {
-    token: token,
-    prefix: prefix,
-    owners: [owner],
-    name: name
+    token: process.env.token,
+    prefix: process.env.prefix,
+    owners: [process.env.owner],
+    name: process.env.name
   },
   apiKeys: {
-    firebase: firebaseApiKey
+    firebase: process.env.firebaseApiKey
   }
 }
