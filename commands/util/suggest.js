87e1003dfe30b0bf022ece02fe358ad7d9e4dc75
Hayden Young <hayden@haydenbjyoung.me>
2018-07-16 00:28:58 +0100

    Changed how the footer is formatted on the embed.

--- commands/util/suggest.js
+++ commands/util/suggest.js
@@ -24,7 +24,7 @@ module.exports = class SuggestCommand extends Command {
       .setTitle('Suggestion!')
       .setColor('#FE5B35')
       .setDescription(suggestion)
-      .setFooter('By ' + msg.member.username + '#' + msg.member.discriminator, msg.member.avatarURL)
+      .setFooter('By ' + msg.author.username + '#' + msg.author.discriminator, msg.author.avatarURL)
       .setTimestamp(new Date().now);
     
     const channel = await msg.guild.channels.find('name', 'suggestions');
