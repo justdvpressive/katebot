ceda57319123211220e7800ccf96c926c6d135f6
Hayden Young <hayden@haydenbjyoung.me>
2018-07-18 13:47:58 +0100

    Added a purge command

---
+++ commands/util/purge.js
@@ -0,0 +1,31 @@
+const {  Command  } = require('discord.js-commando');
+
+module.exports = class PurgeCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'purge',
+      group: 'util',
+      memberName: 'purge',
+      description: 'Deletes a given number of messages from a channel.',
+      userPermissions: [
+        'MANAGE_MESSAGES'
+      ],
+      args: [
+        {
+          key: 'number',
+          prompt: 'How many messages should I delete?',
+          type: 'number',
+          default: ''
+        }
+      ]
+    });
+  }
+
+  run(msg, { number }) {
+    const msgs = msg.channel.fetchMessages({ limit: number || 100 });
+    msgs.forEach(message => {
+      message.delete();
+    });
+    msg.delete();
+  }
+};
