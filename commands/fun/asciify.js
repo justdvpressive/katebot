fe2125ce9bdbdce55098ddb7fc43a511fb6fec27
Hayden Young <hayden@haydenbjyoung.me>
2018-07-18 09:06:36 +0100

    Added an Asciify command

---
+++ commands/fun/asciify.js
@@ -0,0 +1,24 @@
+const { Command } = require('discord.js-commando');
+const figlet      = require('figlet');
+
+module.exports = class AsciifyCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'asciify',
+      group: 'fun',
+      memberName: 'asciify',
+      description: 'Turns text into ascii art!',
+      args: [
+        {
+          key: 'text',
+          prompt: 'What text would you like to \'asciify\'?',
+          type: 'string'
+        }
+      ]
+    });
+  }
+
+  run(msg, {text}) {
+    msg.reply('```\n' + figlet(text) + '```\n');
+  }
+};
